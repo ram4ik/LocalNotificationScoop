da097b45c8f88766616b516b927507858e9098d8
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-09-07 16:40:16 +0300

    Initial Commit

---
+++ LocalNotificationScoop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalNotificationScoop
+//
+//  Created by ramil on 07.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocalNotificationScoop/LocalNotificationScoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationScoopApp.swift
+//  LocalNotificationScoop
+//
+//  Created by ramil on 07.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationScoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
